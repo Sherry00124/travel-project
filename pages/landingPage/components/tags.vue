dc0804544da9237d4b06f78500488c65e042b144
Sherry00124 <511210839@qq.com>
2024-01-22 23:55:35 +0800

    落地页优化

---
+++ pages/landingPage/components/tags.vue
@@ -0,0 +1,60 @@
+<template>
+	<view class="grid-container">
+		<view class="tags" v-for="(baseListItem,index) in tags">
+			<text class="tags-text">{{baseListItem}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	import {
+		shopDetail
+	} from "@/api/shop.js";
+	export default {
+		name: "Tags",
+		props: ['id'],
+		data() {
+			return {
+				tags: []
+			};
+		},
+		mounted() {
+			this.getShopDetail()
+		},
+		methods: {
+			getShopDetail() {
+				shopDetail(this.id).then(res => {
+					this.tags = res.data.intro.split("\n");
+				})
+
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.grid-container {
+	    display: grid;
+	    grid-template-columns: repeat(2, 1fr); /* 将容器分为两列，每列宽度相等 */
+	    grid-gap: 20px; /* 设置列之间的间隔，根据需要进行调整 */
+	    /* 其他样式，如边距等，可以根据需要进行调整 */
+	}
+	.tags {
+		// width: 60%;
+		background-image: url("../../../static/landingPage/bg_label.png");
+		background-repeat: no-repeat;
+		background-size: 100% 100%;
+		color: #fff;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		padding: 10rpx 0;
+		
+		&-text {
+			font-size: 24rpx;
+			font-weight: 600;
+			color: #FFFFFF;
+			text-shadow: 4px 0px 3px #FF9638;
+		}
+	}
+</style>
--- pages/landingPage/landingPage.vue
+++ pages/landingPage/landingPage.vue
@@ -1,193 +1,196 @@
 <template>
-  <view class="container flex-column">
-    <view class="container-header">
-      <div style="margin-top: 50rpx">
-        你有多久 <br />没有好好 <br />去旅行了？
-      </div>
-    </view>
-    <view class="container-body">
-      <view class="container-body-item" v-for="(item, index) in list">
-        <view class="container-body-item-header"> 寻梦巴黎 </view>
-        <div class="container-body-item-body flex-row">
-          <img :src="item.images[0]" width="100" height="100" alt="" />
-          <view class="flex-column">
-            <span class="container-body-item-body-title">{{ item.name }}</span>
-            <view class="container-body-item-tag">
-              <!-- {{ getShopDetail(item.id) }} -->
-              <view
-                class="container-body-item-tags"
-                v-for="(tag, index) in getShopDetail(item.id)"
-                :key="index"
-              >
-                {{ tag }}
-              </view>
-            </view>
+	<view class="container flex-column">
+		<view class="container-header">
+			<div style="position: absolute;top: 30px;left: 25%;">
+				你有多久 <br /><span style="margin-left: 40rpx;">没有好好</span> <br /><span
+					style="margin-left: 60rpx;">去旅行了？</span>
+			</div>
+		</view>
+		<view class="container-body">
+			<view class="container-body-item" v-for="(item, index) in list">
+				<view class="container-body-item-header"> {{item.type_tags}} </view>
+				<div class="container-body-item-body flex-row">
+					<img :src="item.images[0]" width="100" height="100" alt="" />
+					<view class="flex-column" style="justify-content: space-between;">
+						<span class="container-body-item-body-title">{{ item.name }}</span>
+						<view class="container-body-item-tag">
+							<Tags :id="item.id" />
+						</view>
 
-            <view class="flex-between" style="width: 400rpx">
-              <view class="flex-row" style="align-items: center">
-                <view class="container-body-item-body-price"
-                  >￥{{ item.product_price }}</view
-                >
-                <img
-                  src="/static/landingPage/yuan.png"
-                  width="20"
-                  height="20"
-                  alt=""
-                />
-                <span style="font-size: 30rpx; font-weight: 400; color: #7d2f3b"
-                  >起</span
-                >
-              </view>
-              <view class="container-body-item-btn">查看详情</view>
-            </view>
-          </view>
-        </div>
-      </view>
-    </view>
-    <view class="container-footer flex-between">
-      <img :src="qrCode" width="100" alt="" />
-      <img src="/static/landingPage/word.png" width="150" alt="" />
-    </view>
-  </view>
+						<view class="flex-between" style="width: 400rpx">
+							<view class="flex-row" style="align-items: flex-end">
+								<view class="container-body-item-body-price">￥{{ item.product_price }}</view>
+								<img src="/static/landingPage/yuan.png" width="20" height="20" alt="" />
+								<span style="font-size: 25rpx; font-weight: 400; color: #7d2f3b">起</span>
+							</view>
+							<view class="container-body-item-btn" @click="show=true">查看详情</view>
+						</view>
+					</view>
+				</div>
+			</view>
+		</view>
+		<view class="container-footer flex-between">
+			<img :src="qrCode" width="110" alt="" />
+			<img src="/static/landingPage/word.png" width="150" alt="" />
+		</view>
+		<qrCode :show="show" @close="close" />
+	</view>
 </template>
 
 <script>
-import { shopNewList, shopDetail } from "@/api/shop.js";
-import { getConfig } from "@/api/config.js";
-export default {
-  data() {
-    return {
-      baseList: [],
-      list: [],
-      pageNum: 1,
-      totalPage: 1,
-      pageSize: 2,
-      qrCode: "",
-    };
-  },
-  mounted() {
-    this.getShopList();
-    this.getConfigInfo();
-  },
-  methods: {
-    getShopList() {
-      let page = {
-        page: this.pageNum,
-        perPage: this.pageSize,
-      };
-      shopNewList(page).then((res) => {
-        res.list.data.forEach((item) => {
-          this.list.push(item);
-        });
-        uni.hideLoading();
-        this.totalPage = res.list.last_page;
-      });
-    },
-    getConfigInfo() {
-      getConfig().then((res) => {
-        this.qrCode = res.data[0].qrcode;
-      });
-    },
-    getShopDetail(id) {
-      shopDetail(id).then((res) => {
-        return res.data.intro.split("\n");
-      });
-    },
+	import {
+		shopNewList,
+		shopDetail
+	} from "@/api/shop.js";
+	import {
+		getConfig
+	} from "@/api/config.js";
+	// import {Tags} from './components/tags.vue'
+	export default {
+		data() {
+			return {
+				baseList: [],
+				list: [],
+				pageNum: 1,
+				totalPage: 1,
+				pageSize: 2,
+				qrCode: "",
+				show: false
+			};
+		},
+		components: {
+			Tags: () => import("./components/tags.vue"),
+		},
+		mounted() {
+			this.getShopList();
+			this.getConfigInfo();
+		},
+		methods: {
+			getShopList() {
+				let page = {
+					page: this.pageNum,
+					perPage: this.pageSize,
+				};
+				shopNewList(page).then((res) => {
+					// res.list.data.forEach((item) => {
+					// 	this.list.push(item);
+					// });
+					this.list = res.list.data.slice(0, 2)
+					uni.hideLoading();
+					this.totalPage = res.list.last_page;
+				});
+			},
+			getConfigInfo() {
+				getConfig().then((res) => {
+					this.qrCode = res.data[0].qrcode;
+				});
+			},
+			close(params) {
+				this.show = params
+			}
 
-    async itemTags(id) {
-      try {
-        const res = await shopDetail(id);
-        console.log(res);
-        return res.data.intro.split("\n");
-      } catch (error) {
-        console.error("Error fetching shop detail:", error);
-        return [];
-      }
-    },
-  },
-};
+		},
+	};
 </script>
 
 <style lang="scss">
-.container {
-  background-image: url("../../static/landingPage/bg.png");
-  background-repeat: no-repeat;
-  background-size: cover;
-  height: 100%;
-  width: 100%;
-  padding: 20rpx 0rpx;
-  justify-content: center;
-  align-items: center;
-  //   position: relative;
-  &-header {
-    height: 420rpx;
-    color: #fff;
-    font-size: 50rpx;
-    font-weight: bold;
-  }
-  &-footer {
-    // position: fixed;
-    width: 80%;
-    margin-top: 10rpx;
-    // bottom: 10rpx;
-  }
-  &-body {
-    &-item {
-      position: relative;
-      background-color: pink;
-      padding: 20rpx;
-      border-radius: 15rpx;
-      margin-bottom: 20rpx;
-      img {
-        margin-right: 10rpx;
-        border-radius: 16rpx;
-      }
-      &-header {
-        position: absolute;
-        top: 0;
-        left: 50%; /* 设置左边距为50% */
-        transform: translateX(-50%);
-        width: 350rpx;
-        height: 60rpx;
-        color: #fff;
-        background-image: url("../../static/landingPage/bg_title.png");
-        background-repeat: no-repeat;
-        background-size: 100% 100%;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-      &-btn {
-        background-image: url("../../static/landingPage/button.png");
-        background-repeat: no-repeat;
-        background-size: 100% 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: #fff;
-        padding: 10rpx;
-      }
-      &-tags {
-        background-image: url("../../static/landingPage/bg_label.png");
-        background-repeat: no-repeat;
-        background-size: 100% 100%;
-      }
-      &-body {
-        margin-top: 50rpx;
-        &-title {
-          font-size: 36rpx;
-          font-weight: 600;
-          color: #7d2f3b;
-        }
-        &-price {
-          font-size: 36rpx;
-          font-weight: 600;
-          background: radial-gradient(#eba754 0%, #ff6940 50%, #dc5bc3 100%);
-          -webkit-background-clip: text;
-          -webkit-text-fill-color: transparent;
-        }
-      }
-    }
-  }
-}
+	.container {
+		background-image: url("../../static/landingPage/bg.png");
+		background-repeat: no-repeat;
+		background-size: cover;
+		height: 100vh;
+		width: 100%;
+		padding: 20rpx 0rpx;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+
+		//   position: relative;
+		&-header {
+			height: 350rpx;
+			color: #fff;
+			font-size: 75rpx;
+			font-weight: bold;
+			text-shadow: 2px 2px 4px #DB4444;
+		}
+
+		&-footer {
+			width: 80%;
+			margin-top: 30rpx;
+		}
+
+		&-body {
+			&-item {
+				position: relative;
+				background-color: pink;
+				padding: 20rpx;
+				border-radius: 15rpx;
+				margin-bottom: 20rpx;
+				width: 600rpx;
+
+				img {
+					margin-right: 10rpx;
+					border-radius: 16rpx;
+				}
+
+				&-header {
+					position: absolute;
+					top: 0;
+					left: 50%;
+					/* 设置左边距为50% */
+					transform: translateX(-50%);
+					width: 350rpx;
+					height: 70rpx;
+					color: #fff;
+					background-image: url("../../static/landingPage/bg_title.png");
+					background-repeat: no-repeat;
+					background-size: 100% 100%;
+					display: flex;
+					// align-items: center;
+					justify-content: center;
+				}
+
+				&-btn {
+					background-image: url("../../static/landingPage/button.png");
+					background-repeat: no-repeat;
+					background-size: 100% 100%;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					color: #fff;
+					padding: 0 20rpx;
+					font-size: 24rpx;
+				}
+
+				&-body {
+					margin-top: 50rpx;
+
+					&-title {
+						font-size: 36rpx;
+						font-weight: 600;
+						color: #7d2f3b;
+						display: -webkit-box;
+						-webkit-box-orient: vertical;
+						overflow: hidden;
+						text-overflow: ellipsis;
+						-webkit-line-clamp: 2;
+					}
+
+					&-price {
+						font-size: 36rpx;
+						font-weight: 600;
+						background: radial-gradient(#eba754 0%, #ff6940 50%, #dc5bc3 100%);
+						-webkit-background-clip: text;
+						-webkit-text-fill-color: transparent;
+						margin-right: 5rpx;
+					}
+				}
+
+				&-tag {
+					margin: 10rpx 0;
+
+				}
+			}
+		}
+	}
 </style>
